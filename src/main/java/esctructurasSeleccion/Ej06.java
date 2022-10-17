c9c91633fea60ceec5f84a80eb94710385467fb2
elisabet <edacalm990@g.educaand.es>
2022-10-17 09:06:24 +0200

    modificando ejercicio 6 para que haga las operaciones necesarias en el area

--- src/main/java/esctructurasSeleccion/Ej06.java
+++ src/main/java/esctructurasSeleccion/Ej06.java
@@ -4,6 +4,7 @@
  */
 package esctructurasSeleccion;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -12,6 +13,7 @@ import javax.swing.JOptionPane;
  */
 public class Ej06 {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
         Object [] areas={"cuadrado", "triangulo", "circulo", "finalizar"};
         // JOpcion con desplegable
         Object opcion = JOptionPane.showInputDialog(null, 
@@ -30,10 +32,28 @@ public class Ej06 {
         final String TEXTO="Has seleccionado calcular el area de un ";
         if (opcion=="cuadrado") {
             JOptionPane.showMessageDialog(null, TEXTO + opcion);
-        } else if (opcion=="triangulo") {
+            System.out.println("Dame el lado");
+            double ladoCuadrado= teclado.nextDouble();
+            double areaCuadrado= Math.pow(ladoCuadrado, 2);
+            System.out.println("El area del cuadrado es "+areaCuadrado);
+        } 
+        
+        else if (opcion=="triangulo") {
             JOptionPane.showMessageDialog(null, TEXTO + opcion);
+            System.out.println("Dame la altura");
+            double alturaTrinagulo = teclado.nextDouble();
+            System.out.println("Dame la base");
+            double baseTriangulo = teclado.nextDouble();
+            double areaTriangulo = (baseTriangulo*alturaTrinagulo)/2;
+            System.out.println("El area del triangulo es "+alturaTrinagulo);
+            
         } else if (opcion=="circulo") {
             JOptionPane.showMessageDialog(null, TEXTO + opcion);
+            System.out.println("Dame el radio del circulo");
+            double radioCirculo= teclado.nextDouble();
+            double areaCirculo = Math.PI*(Math.pow(radioCirculo, 2));
+            System.out.println("El area del circulo es "+ areaCirculo);
+            
         } else {
             JOptionPane.showMessageDialog(null, "Ha seleccionado terminar");
         }
